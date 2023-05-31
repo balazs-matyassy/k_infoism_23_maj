e76b98d94bf0101016aa1b70e8ae5ec5990ce359
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2023-05-31 08:00:44 +0200

    1. feladat megoldása

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # 1. feladat: adattároló osztály létrehozása
+class Utasszallito:
+    def __init__(self, tipus, ev, utas, szemelyzet, utazosebesseg, felszallotomeg, fesztav):
+        self.tipus = tipus
+        self.ev = ev
+        self.utas = utas
+        self.szemelyzet = szemelyzet
+        self.utazosebesseg = utazosebesseg
+        self.felszallotomeg = felszallotomeg
+        self.fesztav = fesztav
