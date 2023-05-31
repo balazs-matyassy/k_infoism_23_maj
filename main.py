24abe56778fbf43d55089d376f575fdb06448e42
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2023-05-31 08:10:02 +0200

    2. feladat megoldása

--- main.py
+++ main.py
@@ -10,3 +10,18 @@ class Utasszallito:
         self.fesztav = fesztav
 
 # 2. feladat: sorokat (a fájl 1 sorát) példányokká alakító függvény létrehozása
+def sorbol_utasszallito(sor):
+    sor = sor.strip()  # enter levágása a sor végéről
+    ertekek = sor.split(';')
+
+    tipus = ertekek[0]  # szöveg
+    ev = int(ertekek[1])  # egész
+    utas = ertekek[2]  # szöveg
+    szemelyzet = ertekek[3]  # szöveg
+    utazosebesseg = int(ertekek[4])  # egész
+    felszallotomeg = int(ertekek[5])  # egész
+    fesztav = float(ertekek[6].replace(',', '.'))
+
+    utasszallito = Utasszallito(tipus, ev, utas, szemelyzet, utazosebesseg, felszallotomeg, fesztav)
+
+    return utasszallito
